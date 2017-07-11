468c71b7272ea2a2ac0967c87915d0f9d281e0d9
Meechyst <meechysm@windowslive.com>
2017-07-11 16:41:19 +0300

    index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html ng-app="doto" lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Things To Do</title>
+    <link href="favicon.png" rel="shortcut icon">
+    <link href="//fonts.googleapis.com/css?family=Roboto|Montserrat:400,700|Open+Sans:400,300,600" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <link href="{{url('css/app.2b3eb5b149c10b551042.css')}}" rel="stylesheet">
+    <link href="{{url('css/angular-toastr.css')}}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+</head>
+<body>
+<div ui-view="top"></div>
+<div class="container">
+    <div ui-view="content"></div>
+</div>
+<script src="{{mix('js/vendor.js')}}"></script>
+<script src="{{mix('js/app.js')}}"></script>
+</body>
+</html>
