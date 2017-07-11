f36f2cc3f368fac53b56489864c310a872769135
Meechyst <meechysm@windowslive.com>
2017-07-11 16:44:47 +0300

    Routes and bug fixes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,13 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\LikeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\StatusUpdateController;
+use App\Http\Controllers\UserController;
+
+
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +19,29 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+//
+//Route::middleware('auth:api')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'guest'], function () {
+    Route::post('auth/login', [ 'as' => 'login', 'uses' => 'AuthController@login']);
+    Route::post('auth/signup',  'AuthController@signup');
+
+    Route::post('auth/twitter', 'AuthController@twitter');
+    Route::post('auth/facebook', 'AuthController@facebook');
+    Route::post('auth/google', 'AuthController@google');
 });
+
+Route::group(['middleware' => 'auth'], function () {
+
+    Route::get('/user', 'UserController@getUser');
+    Route::get('api/me', 'UserController@getUser');
+    Route::put('api/me', 'UserController@updateUser');
+    Route::get('api/chech/{displayname}', 'UserController@chechUser');
+
+
+    Route::resource('users', 'UserController');
+
+});
+
